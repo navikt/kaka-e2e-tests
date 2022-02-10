c9afb6036599b371dc0d0bab8cf760c1a1a11781
Daniel Eriksson <eriksson.daniel@protonmail.com>
2022-02-10 07:39:08 +0100

    Add navigation test for totaloversikt

---
+++ tests/statistics.navigation.spec.ts
@@ -0,0 +1,19 @@
+import test, { expect } from '@playwright/test';
+import { getParsedUrl } from './functions';
+import { getLoggedInPage } from './helpers';
+import { userSaksbehandler } from './users';
+
+test.describe('Navigation', () => {
+  test.beforeEach(async ({ page }) => {
+    await getLoggedInPage(page, userSaksbehandler);
+  });
+
+  test.only('Totalstatistikk navigates to /statistikk/total', async ({ page }) => {
+    const link = await page.waitForSelector('data-testid=statistikk-total-nav-link', { timeout: 10000 });
+
+    await link.click();
+
+    const url = getParsedUrl(page.url());
+    expect(url.pathname).toBe('/statistikk/total');
+  });
+});
